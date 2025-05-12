bee2eccd3abcf0bc567b09f88441584610033d4e
subhamsam007 <mondalsubham801@gmail.com>
2025-05-12 20:55:58 +0530

    neww function open new page

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var cards = document.querySelectorAll('.custom-card');
-var sections = document.querySelectorAll('.inside');
+var sections = document.querySelectorAll('.inside-section');
 
-
-cards.forEach(function(card) {
-    card.addEventListener("click", function(){
-        sections[card.id].style.display = "block";
-    })
+cards.forEach(function(card){
+    card.addEventListener('click', function() {
+        sections[card.id].style.display = 'block';
+    });
 });
