972d1904a7e74477de0f7cb6bcb011c8f3d5cf98
subhamsam007 <mondalsubham801@gmail.com>
2025-05-15 16:23:21 +0530

    back function

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var cards = document.querySelectorAll('.custom-card');
 var sections = document.querySelectorAll('.inside-section');
+var closeButtons = document.querySelectorAll('.close-btn');
 
 cards.forEach(function(card){
     card.addEventListener('click', function() {
         sections[card.id].style.display = 'block';
+    });
+});
+closeButtons.forEach((btn)=>{
+   btn.addEventListener('click', function() {
+        sections[btn.id].style.display = 'none';
     });
 });
