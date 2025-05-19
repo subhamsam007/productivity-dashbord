b4f4e427659d2285dadbdc9b51ebd4508c6c3734
subhamsam007 <mondalsubham801@gmail.com>
2025-05-20 01:26:09 +0530

    js changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var cards = document.querySelectorAll('.custom-card');
+function openFunction(){
+    var cards = document.querySelectorAll('.custom-card');
 var sections = document.querySelectorAll('.inside-section');
 var closeButtons = document.querySelectorAll('.close-btn');
 
@@ -12,3 +13,34 @@ closeButtons.forEach((btn)=>{
         sections[btn.id].style.display = 'none';
     });
 });
+};
+openFunction();
+const formTododo = document.querySelector(".todo-form");
+const formInput = document.querySelector(".todo-form-input");
+const formButton = document.querySelector(".todo-form-button");    
+const taskDetails = document.querySelector(".todo-form-text");
+
+formTododo.addEventListener("submit", function(e){
+    e.preventDefault();
+    const task = formInput.value;
+    const details = taskDetails.value;
+    
+})
+
+const currentTask =[{
+    task: "Task 1",
+    details: "Details of Task 1",
+    status: "completed"
+},
+{
+    task: "Task 2",
+    details: "Details of Task 2",
+    status: "pending"
+}
+];
+
+currentTask.forEach((task) => {
+ 
+    const taskList =document.querySelector(".task-list");
+    console.log(taskList.value);
+})
